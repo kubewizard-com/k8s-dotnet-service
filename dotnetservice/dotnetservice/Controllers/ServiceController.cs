e1422fd481e13a35b0b485486014ef58e669d0b8
Krzysztof Wasiak <k.wasiak@gmail.com>
2018-08-17 21:21:31 +0100

    Initial commit

---
+++ dotnetservice/dotnetservice/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using dotnetservice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnetservice.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public ServiceController(ILogger<ServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("ping")]
+        public PongMessage Ping(string message)
+        {
+            _logger.LogInformation("Got ping request. Message: {}", message);
+            return new PongMessage(message);
+        }
+    }
+}
---
+++ dotnetservice/dotnetservice/Models/PongMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnetservice.Models
+{
+    public class PongMessage
+    {
+        public string Message { get; }
+        public DateTime Timestamp { get; }
+
+        public PongMessage(string message)
+        {
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
+    }
+}
